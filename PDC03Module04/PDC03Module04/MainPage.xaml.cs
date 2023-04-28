51d4805e772e6fc75805825521cc95a8161969ef
lopezkarylle <dr.lopezkarylle@gmail.com>
2023-04-28 10:54:39 +0800

    first commit

---
+++ PDC03Module04/PDC03Module04/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03Module04
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OpenActivity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity01());
+        }
+        private async void OpenActivity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity02());
+        }
+    }
+}
---
+++ PDC03Module04/PDC03Module04/activity02.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03Module04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity02 : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+        public activity02()
+        {
+            InitializeComponent();
+            EmployeeView.ItemsSource = employees;
+
+            employees.Add(new Employee { DisplayName = "Im Nayeon", Position = "Chief Executive Officer" });
+            employees.Add(new Employee { DisplayName = "Yoo Jeongyeon", Position = "Chief Operating Officer" });
+            employees.Add(new Employee { DisplayName = "Hirai Momo", Position = "Chief Financial Officer" });
+            employees.Add(new Employee { DisplayName = "Minatozaki Sana", Position = "Chief Marketing Officer" });
+            employees.Add(new Employee { DisplayName = "Park Jihyo", Position = "Marketing Manager" });
+            employees.Add(new Employee { DisplayName = "Myoui Mina", Position = "Sales Manager" });
+        
+         }
+
+        private void Add_Item(object sender, EventArgs e)
+        {
+            
+        }
+        private void Edit_Item(object sender, EventArgs e)
+        {
+
+        }
+        private void Delete_Item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            { 
+            
+            }
+        }
+    }
+}
